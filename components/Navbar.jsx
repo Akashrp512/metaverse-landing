cb82b9b5be2b7fbf50d5bf7a3f0d34d5cbd5d254
Akash RP <akashrp512@gmail.com>
2022-11-25 21:48:04 +0530

    commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,18 @@
 'use client';
 
+import {motion} from "framer-motion";
+import styles from '../styles';
+import {navVariants} from  '../utils/motion';
+
 const Navbar = () => (
-  <nav>
+  <motion.nav
+  variants={navVariants}
+  initial = "hidden"
+  whileInView="show"
+  className={`${styles.xPaddings} py-8 relative`}
+  >
     navbar
-  </nav>
+  </motion.nav>
 );
 
 export default Navbar;
