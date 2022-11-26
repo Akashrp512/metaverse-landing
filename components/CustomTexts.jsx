99953483c861bedba1e10888e023610a582c9683
Akash RP <akashrp512@gmail.com>
2022-11-26 13:48:47 +0530

    Explore commit

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -4,7 +4,7 @@ import {motion} from "framer-motion";
 
 import { textContainer, textVariant2 } from "../utils/motion";
 
-export const TypingText = (title,textStyles) => (
+export const TypingText = ({title, textStyles}) => (
   <motion.p
   variants={textContainer}
   className={`font-normal text-[14px] text-secondary-white ${textStyles}` }
@@ -18,6 +18,11 @@ export const TypingText = (title,textStyles) => (
   </motion.p>
 );
 
-export const TitleText = (title, textStyles) => (
-  <h2>Title Text</h2>
+export const TitleText = ({title, textStyles}) => (
+  <motion.h2
+  variants={textVariant2}
+  initial="hidden"
+  whileInView="show"
+  className={`mt-[8px] font-bold md:text-[64px] text-[40px] text-white ${textStyles}`}
+  >{title}</motion.h2>
 );
